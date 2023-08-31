e770987db47c8fbaa9ce34e36b870d4c08710137
horror0110 <133776163+horror0110@users.noreply.github.com>
2023-08-31 21:07:25 +0800

    end

--- app/components/Modal/Modal.jsx
+++ app/components/Modal/Modal.jsx
@@ -34,32 +34,32 @@ const Modal = () => {
 
 
   return (
-    
-       <div className="flex flex-col items-center justify-center">
+   <div className="fixed flex">
 
-        <Shadow/>
 
-<div className={openModal ? "w-[300px] h-auto bg-[teal] rounded-xl text-black p-5 fixed top-24 z-50 " : "hidden"}>
-     
-     
-       
-     <h1>Edit this task  <strong className="font-bold text-yellow-400"> "{getEditTaskValue}"</strong></h1>
+    <Shadow/>
 
-     <input
-       onChange={(e) => setModalItem(e.target.value)}
-       className="bg-mainColor rounded-md w-auto py-2 px-2 my-5 text-white placeholder:text-white"
-       type="text"
-       placeholder="edit task"
-       value={modalItem}
-     />
 
-     <div className="flex justify-between items-center">
-       <button className="bg-red-600 px-2 py-1 rounded-md text-white" onClick={toggleModal}>CANCEL</button>
-       <button className="bg-green-600 px-2 py-1 rounded-md text-white " onClick={editTask}>SAVE</button>
-     </div>
+      {openModal ?    <div className="flex flex-col gap-3 items-center mt-40 p-2 rounded-md bg-[teal] w-[300px] h-auto z-50 ">
+
+            <h1 >Үүнийг засна уу  <strong className="font-bold text-yellow-800"> "{getEditTaskValue}"</strong></h1>
+
+            <input className="rounded-md px-4 py-1 bg-[teal] border-[3px] border-green-500" onChange={(e)=>setModalItem(e.target.value)} type="text" placeholder="засах" />
+
+            <div className="flex items-center gap-10">   
+
+              <button className="bg-green-500 px-2 py-1 rounded-md" onClick={editTask}>Хадгалах</button>
+              <button className="bg-red-500 px-2 py-1 rounded-md" onClick={toggleModal}>Болих</button>
+
+              </div>
+
+             
+         </div> : null}
+
    </div>
-
-       </div>
+  
+    
+   
 
 
 
